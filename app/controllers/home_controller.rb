923e51c5abfda2fc78001b71c461d149baac3ad8
Rodrigo Toledo <rodrigotoledo@192.168.0.102>
2020-09-15 04:31:12 -0300

    testing just the valid of product error on has one last payment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.order_by(name: 'asc')
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,15 @@
 class Product
   include Mongoid::Document
+  include Mongoid::Timestamps::Created::Short
+  include Mongoid::Timestamps::Updated::Short
+
   field :name, type: String
   field :image, type: String
   field :url, type: String
+  
   belongs_to :store
   has_many :payment_systems, autosave: true
-  has_one :last_payment,
+  has_one :last_payment, -> { order_by(created_at: 'desc') },
     class_name: 'PaymentSystem', foreign_key: :product_id
 
   validates :name, :image, :url, :store, presence: true
